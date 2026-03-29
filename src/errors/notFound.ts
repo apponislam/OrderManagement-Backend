a8cc5ee99fb825268e4f5be184a921bbb4d3e8b1
Appon Islam <appon2003@gmail.com>
2026-03-30 02:11:42 +0600

    full backend updated

--- src/errors/notFound.ts
+++ src/errors/notFound.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.url);
     res.status(httpStatus.NOT_FOUND).json({
         success: false,
         message: "API Not Found !!",
